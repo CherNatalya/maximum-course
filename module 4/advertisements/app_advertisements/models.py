ea2ffc023f526c180426b37b66f3bed910c082b1
Natalya <chernovvol.natalya@gmail.com>
2023-08-27 15:52:01 +0300

    lesson 8 (Administration panel)

--- module 4/advertisements/app_advertisements/models.py
+++ module 4/advertisements/app_advertisements/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib import admin
 
 
-class Advertisements(models.Model):
+class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField("Описание")
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
@@ -14,3 +15,26 @@ class Advertisements(models.Model):
 
     def __str__(self):
         return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weght: bold">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")\
+
+
+    @admin.display(description="Дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: blue; font-weght: bold">Сегодня в {}</span>', updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
