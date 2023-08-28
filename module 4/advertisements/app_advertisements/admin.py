37094cf0c9f945276cd185068cdc487b8a0fe7c0
Natalya <chernovvol.natalya@gmail.com>
2023-08-28 22:08:03 +0300

    lesson 9 (Наполнение Шаблонов)

--- module 4/advertisements/app_advertisements/admin.py
+++ module 4/advertisements/app_advertisements/admin.py
@@ -3,16 +3,17 @@ from .models import Advertisement
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "description", "price", "created_date", "updated_date", "auction"]
+    list_display = ["id", "title", "description", "price", "created_date", "updated_date", "auction", "preview_image"]
     list_filter = ["auction", "created_at", "updated_at"]
     actions = ["make_auction_as_false", "make_auction_as_true"]
     fieldsets = (
-        ("Общие", {"fields": ("title", "description")}),
+        ("Общие", {"fields": ("title", "description", "image", "preview_image")}),
         ("Финансы", {
             "fields": ("price", "auction"),
             "classes": ["collapse"]
         })
     )
+    readonly_fields = ["preview_image", ]
 
     @admin.action(description="Убрать возможность торга")
     def make_auction_as_false(self, request, queryset):
--- module 4/advertisements/app_advertisements/models.py
+++ module 4/advertisements/app_advertisements/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib import admin
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+from django.conf.urls.static import static
+User = get_user_model()
+img = "E:\My_projects_python\maximum-course\module 4\advertisements\static\img\adv.png"
 
 
 class Advertisement(models.Model):
@@ -9,6 +14,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text="Отметьте, если будет уместен торг")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    image = models.ImageField("Изображение", upload_to="advertisements/")
 
     class Meta:
         db_table = 'advertisements'
@@ -19,22 +26,25 @@ class Advertisement(models.Model):
     @admin.display(description="Дата создания")
     def created_date(self):
         from django.utils import timezone
-        from django.utils.html import format_html
         if self.created_at.date() == timezone.now().date():
             created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
                 '<span style="color: green; font-weght: bold">Сегодня в {}</span>', created_time
             )
-        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")\
-
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     @admin.display(description="Дата обновления")
     def updated_date(self):
         from django.utils import timezone
-        from django.utils.html import format_html
         if self.updated_at.date() == timezone.now().date():
             updated_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html(
                 '<span style="color: blue; font-weght: bold">Сегодня в {}</span>', updated_time
             )
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description="Предпросмотр изображения")
+    def preview_image(self):
+        if self.image:
+            return format_html('<img src=" {} " width="40">', self.image.url)
+        return format_html('<img src=" {} " width="40">', 'http://127.0.0.1:8000/media/advertisements/adv.png')
